fbc930fab3503fcaa5ae86c19dec943b8dde5bb2
Jo <joachim.dev@pm.me>
2025-06-29 23:41:11 +0200

    Update .config/hypr/config/monitor.conf Update .config/fish/config.fish Update .ssh/config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,3 +1,12 @@
+# SSH functions for local servers
+function birdstrike-vm
+    sshpass -f ~/.ssh/local_servers_password ssh -A birdstrike-vm $argv
+end
+
+function birdstrike-ai
+    sshpass -f ~/.ssh/local_servers_password ssh -A birdstrike-ai $argv
+end
+
 status is-interactive; and begin
 
     # Abbreviations
@@ -7,6 +16,10 @@ status is-interactive; and begin
     alias cd z
     alias cdi zi
     alias wp-switch 'swww img $(fd --extension "gif" --follow . ~/Pictures/Wallpapers/ | fzf --preview='\''preview_gif {}'\'')'
+
+    # Short aliases
+    alias s1='birdstrike-vm'
+    alias s2='birdstrike-ai'
 
     set fish_greeting # Disable greeting
 
